8fb75a21493fa6ed79be298c6e06ac40bb8f33c1
Igor Karpov <igorok.karpov@gmail.com>
2020-01-08 17:48:52 +0200

    init commit

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**
+ * Project: ${PROJECT_NAME}
+ * Problem URL:
+ * Difficulty: Easy
+ * Description: 
+ * Author: Igor Karpov <igorok.karpov@gmail.com>
+ * Date: ${DATE}
+ * Time: ${TIME}
+ */
---
+++ .idea/fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,12 @@
+/**
+ * Project: ${PROJECT_NAME}
+ * Problem URL:
+ * Difficulty: Easy
+ * Description: 
+ * Author: Igor Karpov <igorok.karpov@gmail.com>
+ * Date: ${DATE}
+ * Time: ${TIME}
+ * Class ${NAME}
+ #if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+  */
