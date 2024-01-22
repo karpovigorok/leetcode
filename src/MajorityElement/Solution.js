af03bd05667a4cede5208767f5652dc6d3c5d813
Igor Karpov <igor.karpov@kstt.ua>
2024-01-22 17:13:18 +0100

    solve problem js

--- src/MajorityElement/Solution.js
+++ src/MajorityElement/Solution.js
@@ -1,4 +1,7 @@
 /**
+ *
+ * https://leetcode.com/problems/majority-element/description/
+ *
  * 1. sort and find middle element of array. Time: O(n log n)
  * 2. Moore Voting Algorithm O(n)
  * The intuition behind the Moore's Voting Algorithm is based on the fact that if there is a majority element in an array, it will always remain in the lead, even after encountering other elements.
@@ -7,7 +10,7 @@
  * @param {number[]} A
  * @return {number}
  */
-var majorityElement = function(A) {
+let majorityElement = function(A) {
     let majority = 0;
     let count = 0;
     for(let i = 0; i < A.length; i++) {
